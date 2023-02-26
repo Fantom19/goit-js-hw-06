68d4c8a97513039a95ea8802d37a62fe49fa8588
Kirill <fantomkirill44@gmail.com>
2023-02-26 16:04:51 +0200

    hw-6

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,50 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+             const inputNumEl = document.querySelector('#controls input');
+             const boxesEl = document.querySelector('#boxes');
+             const btnCreate = document.querySelector('[data-create]');
+             const btnDestroy = document.querySelector('[data-destroy]');
+
+             boxesEl.style.cssText = "display:flex;flex-wrap:wrap; gap:5px";
+
+             let width = 30;
+             let height = 30;
+
+                   btnCreate.addEventListener('click', event => { 
+                     const newNumber = Number(inputNumEl.value);
+                     if (newNumber < inputNumEl.min || newNumber > inputNumEl.max) {
+                      inputNumEl.value = '';
+                      return alert(
+                        `Enter a number in the range ${inputNumEl.min} to ${inputNumEl.max}`,
+                      );
+                   });
+                  
+                   btnDestroy.addEventListener('click', event => {
+  
+                     const divsElBoxes = boxesEl.querySelectorAll('div');
+                     divsElBoxes.forEach(a => a.remove());
+                   });
+
+                   const createBoxes = amount => {
+                     const newDiv = [];
+
+                     for (let i = 0; i < amount; i++) {
+                       const createDivEl = document.createElement('div');
+                       newDiv.push(createDivEl);
+                     }
+
+                     boxesEl.append(
+                       ...newDiv.map(cube => {
+                        cube.style.width = `${width}px`;
+                        cube.style.height = `${height}px`;
+                        cube.style.backgroundColor = getRandomHexColor();
+                         width += 10;
+                         height += 10;
+                         return cube;
+                       })
+                     );
+                   };
+
+            
